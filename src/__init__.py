bf60cb36be65894f204c9b82ea78442537a8f527
Ying <ying.liu@yingliu.site>
2025-05-15 09:52:43 +0100

    [fix] change python version

--- src/__init__.py
+++ src/__init__.py
@@ -3,4 +3,6 @@ Human Pose Estimation (HPE) Pretraining System
 
 A production-level ML system for unsupervised pretraining of Human Pose Estimation models,
 designed to predict 3D human pose from 2D keypoints or 3D keypoitns.
-""" 
+"""
+
+__version__ = "0.1.0" 
